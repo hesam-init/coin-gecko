35b55874fafc2c30e1e5b7864d27dc1dd9015aab
Hesam Yusefpur <hesam.init@gmail.com>
2022-04-11 19:02:14 +0430

    [UPDATE] : scroll animation added

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,21 +1,35 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./coin.css";
 
 const Coin = (props) => {
+  // aos
+  useEffect(() => {
+    AOS.init({
+      offset: 0,
+      duration: 500,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <div
-      className="shadow-effect rounded-lg p-3 bg-white flex items-center gap-5 transition-all duration-300 ease-in-out hover:cursor-pointer hover:translate-y-1 hover:shadow h-32"
-      id={props.id}
-    >
-      <img
-        src={props.img}
-        alt={props.id}
-        title={props.name}
-        className="w-16 bg-white img-shadow rounded-full"
-      />
-      <span className="text-black">
-        {props.name}
-        {props.price}
-      </span>
+    <div data-aos="fade-down" data-aos-once="true">
+      <div
+        className="shadow-effect rounded-lg p-3 bg-white flex items-center gap-5 transition-all duration-300 ease-in-out hover:cursor-pointer hover:translate-y-1 hover:shadow h-24"
+        id={props.id}
+      >
+        <LazyLoadImage
+          alt={props.id}
+          src={props.img}
+          className="w-12 bg-white img-shadow rounded-full"
+        />
+        <span className="text-black">
+          {props.name}
+          {props.price}
+        </span>
+      </div>
     </div>
   );
 };
