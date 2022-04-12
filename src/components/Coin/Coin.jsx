782d2063e280dba0ff60c8d6b1c7ee0142a5c5ce
Hesam Yusefpur <hesam.init@gmail.com>
2022-04-12 23:01:47 +0430

    [UPDATE] : aos removed cause shit performance

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,24 +1,12 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
-import AOS from "aos";
 import "aos/dist/aos.css";
 import "./coin.css";
-import { useLayoutEffect } from "react";
 
 const Coin = (props) => {
-  useLayoutEffect(() => {
-    AOS.init({
-      anchorPlacement: "top-bottom",
-      duration: 850,
-      once: true,
-      offset: 0,
-    });
-  }, []);
-
   return (
     <Link to={`/coin/${props.id}`}>
       <div
-        data-aos="flip-down"
         className="shadow-effect rounded-lg p-3 bg-white flex items-center gap-5 transition-all duration-300 ease-in-out hover:cursor-pointer hover:translate-y-1 hover:shadow h-24"
         id={props.id}
       >
