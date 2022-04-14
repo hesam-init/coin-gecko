ec76199f0e034033928ba3ee61ea0d25fa771fb1
Hesam Yusefpur <hesam.init@gmail.com>
2022-04-14 20:10:41 +0430

    [UPDATE] : motion effect for Coin component

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,5 +1,6 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import "./coin.css";
 
 const Coin = (props) => {
@@ -14,24 +15,28 @@ const Coin = (props) => {
   };
 
   return (
-    <Link to={`/coin/${id}`}>
-      <div
-        data-aos="fade-up"
-        className="shadow-effect rounded-lg p-3 px-4 flex items-center gap-5 transition-all duration-300 ease-in-out hover:cursor-pointer hover:translate-y-1 hover:shadow h-24"
-        id={id}
-      >
-        <LazyLoadImage
-          alt={id}
-          src={img}
-          className="w-9 md:w-10 lg:w-12 bg-white img-shadow rounded-full"
-        />
-        <h1 className="text-white flex items-center gap-3 font-bold">
-          <span>{convertText(symbol)}</span>
-          <span>{price} $</span>
-          <span>Rank : {marketcaprank}</span>
-        </h1>
-      </div>
-    </Link>
+    <motion.div
+      transition={{ duration: 0.5, delay: 0.1 }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+    >
+      <Link to={`/coin/${id}`}>
+        <div
+          className="shadow-effect rounded-lg p-3 px-4 flex items-center gap-5 transition-all duration-300 ease-in-out hover:cursor-pointer hover:translate-y-1 hover:shadow h-24"
+          id={id}
+        >
+          <LazyLoadImage
+            src={img}
+            className="w-9 md:w-10 lg:w-12 bg-white img-shadow rounded-full"
+          />
+          <h1 className="text-white flex items-center gap-3 font-bold">
+            <span>{convertText(symbol)}</span>
+            <span>{price} $</span>
+            <span>Rank : {marketcaprank}</span>
+          </h1>
+        </div>
+      </Link>
+    </motion.div>
   );
 };
 
