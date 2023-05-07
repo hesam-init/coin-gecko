ba5f369ec23403b0a6bc1d1aebfedff9fc1a790f
hesam-init <hesam.init@gmail.com>
2023-05-08 01:44:16 +0330

    Refactor remove cra useless tools out of project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 import App from "./container/App";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-console.log("Crypto App");
-
 root.render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </BrowserRouter>
 );
-
-reportWebVitals();
