cdd0cba5db8216bca3ec3428ad2f3f38b193cbca
hesam-init <hesam.init@gmail.com>
2023-05-08 02:16:16 +0330

    Fix style bug

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: "#ffff",
+      "dracula-main": "#282a36",
+      "dracula-primary": "#44475a",
+    },
+  },
+  plugins: [],
+};
